7e6a55012ce2b0c7855544f74d1efd725ed6d548
pbnoxious <pbnoxious@web.de>
2019-05-14 23:39:41 +0200

    Initial commit

---
+++ src/youtube-to-mpd.py
@@ -0,0 +1,10 @@
+"""Scripts to automatically fetch youtube audio and play it via mpd."""
+
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
